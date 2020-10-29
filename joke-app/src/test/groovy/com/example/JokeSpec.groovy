6df1fc1ab856cf175766502811e6799dd1f5e5ec
Iván López <lopez.ivan@gmail.com>
2020-10-29 17:27:41 +0100

    App (2.1.2): Initial commit

---
+++ joke-app/src/test/groovy/com/example/JokeSpec.groovy
@@ -0,0 +1,19 @@
+package com.example
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import javax.inject.Inject
+
+@MicronautTest
+class JokeSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
